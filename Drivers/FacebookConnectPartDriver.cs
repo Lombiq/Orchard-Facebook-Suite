82785b2db1fb72dc9c84339cca0732648c9726f3
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2011-11-01 19:53:26 +0100

    Proper factory in driver

--- Drivers/FacebookConnectPartDriver.cs
+++ Drivers/FacebookConnectPartDriver.cs
@@ -37,59 +37,64 @@ namespace Piedone.Facebook.Suite.Drivers
 
         protected override DriverResult Display(FacebookConnectPart part, string displayType, dynamic shapeHelper)
         {
-            bool isAuthenticated = _authenticationService.GetAuthenticatedUser() != null;
-            var currentUserPart = _facebookConnectService.GetAuthenticatedFacebookUserPart();
-            bool isConnected = currentUserPart != null;
-            
-            dynamic CurrentUser = new ExpandoObject();
-
-            if (!isAuthenticated)
-            {
-                string[] permissions = FacebookConnectHelper.PermissionSettingsToArray(part.Permissions);
-
-                if (part.AutoLogin)
+            return ContentShape("Parts_FacebookConnect",
+                () =>
                 {
-                    isConnected = _facebookConnectService.Authorize(
-                        permissions: FacebookConnectHelper.PermissionSettingsToArray(part.Permissions),
-                        onlyAllowVerified: part.OnlyAllowVerified);
-                    // string loginUrl = "https://www.facebook.com/dialog/oauth?client_id=" +  _facebookSuiteService.FacebookSuiteSettingsPart.AppId + "&redirect_uri=YOUR_URL&scope=email,read_stream";
+                    bool isAuthenticated = _authenticationService.GetAuthenticatedUser() != null;
+                    var currentUserPart = _facebookConnectService.GetAuthenticatedFacebookUserPart();
+                    bool isConnected = currentUserPart != null;
+
+                    dynamic CurrentUser = new ExpandoObject();
+
+                    if (!isAuthenticated)
+                    {
+                        string[] permissions = FacebookConnectHelper.PermissionSettingsToArray(part.Permissions);
+
+                        if (part.AutoLogin)
+                        {
+                            isConnected = _facebookConnectService.Authorize(
+                                permissions: FacebookConnectHelper.PermissionSettingsToArray(part.Permissions),
+                                onlyAllowVerified: part.OnlyAllowVerified);
+                            // string loginUrl = "https://www.facebook.com/dialog/oauth?client_id=" +  _facebookSuiteService.FacebookSuiteSettingsPart.AppId + "&redirect_uri=YOUR_URL&scope=email,read_stream";
+                            if (isConnected)
+                            {
+                                currentUserPart = _facebookConnectService.GetAuthenticatedFacebookUserPart();
+                            }
+                        }
+                    }
+
                     if (isConnected)
                     {
-                        currentUserPart = _facebookConnectService.GetAuthenticatedFacebookUserPart();
+                        CurrentUser.Name = currentUserPart.Name;
+                        CurrentUser.PictureLink = currentUserPart.PictureLink;
+                        CurrentUser.Link = currentUserPart.Link;
                     }
-                }
-            }
 
-            if (isConnected)
-            {
-                CurrentUser.Name = currentUserPart.Name;
-                CurrentUser.PictureLink = currentUserPart.PictureLink;
-                CurrentUser.Link = currentUserPart.Link;
-            }
-
-
-            return ContentShape("Parts_FacebookConnect",
-                () => shapeHelper.Parts_FacebookConnect(
-                                        IsAuthenticated: isAuthenticated,
-                                        IsConnected: isConnected,
-                                        Permissions: part.Permissions,
-                                        OnlyAllowVerified: part.OnlyAllowVerified,
-                                        CurrentUser: CurrentUser));
+                    return shapeHelper.Parts_FacebookConnect(
+                                IsAuthenticated: isAuthenticated,
+                                IsConnected: isConnected,
+                                Permissions: part.Permissions,
+                                OnlyAllowVerified: part.OnlyAllowVerified,
+                                CurrentUser: CurrentUser);
+                });
         }
 
         // GET
         protected override DriverResult Editor(FacebookConnectPart part, dynamic shapeHelper)
         {
-            if (!_facebookSuiteService.AppSettingsSet)
-            {
-                _notifier.Add(NotifyType.Error, T("Currently the Facebook app settings are not set, therefore Facebook Connect won't work properly. Please set the app settings first on the page Facebook Suite Settings under Settings."));
-            }
-
             return ContentShape("Parts_FacebookConnect_Edit",
-                () => shapeHelper.EditorTemplate(
-                    TemplateName: "Parts/FacebookConnect",
-                    Model: part,
-                    Prefix: Prefix));
+                () =>
+                {
+                    if (!_facebookSuiteService.AppSettingsSet)
+                    {
+                        _notifier.Add(NotifyType.Error, T("Currently the Facebook app settings are not set, therefore Facebook Connect won't work properly. Please set the app settings first on the page Facebook Suite Settings under Settings."));
+                    }
+
+                    return shapeHelper.EditorTemplate(
+                        TemplateName: "Parts/FacebookConnect",
+                        Model: part,
+                        Prefix: Prefix);
+                });
         }
 
         // POST
