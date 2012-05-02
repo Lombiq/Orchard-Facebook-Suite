6acc2add3a93af34b64d01ded3efab52d4d40ad8
piedone@pyrocenter.hu <piedone@pyrocenter.hu>
2012-05-02 10:58:47 +0200

    Redirecting to clean url if facebook_comment_id param is in url (also issue 9)

---
+++ Filters/FacebookCommentsFilter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Orchard.Mvc.Filters;
+using Orchard.Environment.Extensions;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Piedone.Facebook.Suite.Filters
+{
+    [OrchardFeature("Piedone.Facebook.Suite.CommentsBox")]
+    public class FacebookCommentsFilter : FilterProvider, IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+        }
+
+        public void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // This strips out the fb_comment_id parameter from the URL and redirects to it.
+            // This param is added to the URL of the link on FB on a user's timeline when she/he comments on a page.
+            var request = filterContext.HttpContext.Request;
+
+            if (request.QueryString["fb_comment_id"] != null)
+            {
+                var url = request.Url;
+                var queryParams = HttpUtility.ParseQueryString(url.Query);
+                if (queryParams["fb_comment_id"] != null)
+                {
+                    queryParams.Remove("fb_comment_id");
+                }
+                
+                var cleanUrl = url.Scheme + "://" + url.Authority + url.AbsolutePath + ((queryParams.Count != 0) ? "?" + queryParams : "");
+
+                filterContext.Result = new RedirectResult(cleanUrl, true);
+            }
+        }
+    }
+}
